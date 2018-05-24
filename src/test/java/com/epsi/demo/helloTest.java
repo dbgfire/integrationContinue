b5966bca66d6b884054708fe43b40c6e4e7de1c5
dbgfi <alandu56440@hotmail.fr>
2018-05-24 08:55:20 +0200

    test unitaire

--- src/test/java/com/epsi/demo/helloTest.java
+++ src/test/java/com/epsi/demo/helloTest.java
@@ -16,17 +16,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class helloTest {
     @Autowired
     private MockMvc mvc;
-/*
+
     @Test
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Hello it's me!!!")));
-    }*/
+    }
 }
